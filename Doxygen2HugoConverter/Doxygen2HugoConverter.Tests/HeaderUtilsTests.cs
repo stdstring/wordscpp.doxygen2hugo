13a63e361080597ba83c414ffe1ed2b038fea96a
stdstring <std_string@mail.ru>
2023-03-01 15:51:59 +0500

    added header utils + tests on their

---
+++ Doxygen2HugoConverter/Doxygen2HugoConverter.Tests/HeaderUtilsTests.cs
@@ -0,0 +1,103 @@
+﻿using NUnit.Framework;
+
+namespace Doxygen2HugoConverter.Tests
+{
+    [TestFixture]
+    public class HeaderUtilsTests
+    {
+        [Test]
+        public void ParseDataTests()
+        {
+            const String source = "---\r\n" +
+                                  "title: Aspose.Words for C++\r\n" +
+                                  "linktitle: Aspose.Words for C++\r\n" +
+                                  "type: docs\r\n" +
+                                  "weight: 10\r\n" +
+                                  "url: /cpp/\r\n" +
+                                  "keywords: \"Aspose.Words for C++, Aspose Words, Aspose API Reference.\"\r\n" +
+                                  "description: \"Aspose.Words is a class library that can be used by developers for various platforms for a variety of document processing tasks.\"\r\n" +
+                                  "is_root: true\r\n" +
+                                  "---";
+            IDictionary<String, String> expectedData = new Dictionary<String, String>
+            {
+                {"title", "Aspose.Words for C++"},
+                {"linktitle", "Aspose.Words for C++"},
+                {"type", "docs"},
+                {"weight", "10"},
+                {"url", "/cpp/"},
+                {"keywords", "\"Aspose.Words for C++, Aspose Words, Aspose API Reference.\""},
+                {"description", "\"Aspose.Words is a class library that can be used by developers for various platforms for a variety of document processing tasks.\""},
+                {"is_root", "true"}
+            };
+            Assert.That(HeaderUtils.ParseData(source), Is.EquivalentTo(expectedData));
+        }
+    }
+
+    [TestFixture]
+    public class HeaderTemplateTests
+    {
+        [Test]
+        public void CreateFromSourceTest()
+        {
+            const String source = "---\r\n" +
+                                  "title: $title\r\n" +
+                                  "linktitle: $linktitle\r\n" +
+                                  "second_title: Aspose.Words for C++ API Reference\r\n" +
+                                  "description: $description in C++.\r\n" +
+                                  "type: docs\r\n" +
+                                  "weight: $weight\r\n" +
+                                  "url: /cpp/$ref/\r\n" +
+                                  "---";
+            Func<IDictionary<String, String>, String> generatorFunc = HeaderTemplateFactory.CreateFromSource(source);
+            const String expected = "---\r\n" +
+                                    "title: 'Document::Accept method'\r\n" +
+                                    "linktitle: Accept\r\n" +
+                                    "second_title: Aspose.Words for C++ API Reference\r\n" +
+                                    "description: Document method. Accepts a visitor in C++.\r\n" +
+                                    "type: docs\r\n" +
+                                    "weight: 14\r\n" +
+                                    "url: /cpp/aspose.words/document/accept/\r\n" +
+                                    "---";
+            Assert.AreEqual(expected, generatorFunc(new Dictionary<String, String>
+            {
+                {"title", "Document::Accept method"},
+                {"linktitle", "Accept"},
+                {"description", "Document method. Accepts a visitor"},
+                {"weight", "14"},
+                {"ref", "aspose.words/document/accept"}
+            }));
+        }
+
+        [Test]
+        public void CreateFromSourceWithColonInDescriptionTest()
+        {
+            const String source = "---\r\n" +
+                                  "title: $title\r\n" +
+                                  "linktitle: $linktitle\r\n" +
+                                  "second_title: Aspose.Words for C++ API Reference\r\n" +
+                                  "description: $description in C++.\r\n" +
+                                  "type: docs\r\n" +
+                                  "weight: $weight\r\n" +
+                                  "url: /cpp/$ref/\r\n" +
+                                  "---";
+            Func<IDictionary<String, String>, String> generatorFunc = HeaderTemplateFactory.CreateFromSource(source);
+            const String expected = "---\r\n" +
+                                    "title: 'Aspose::Words::Document::Accept method'\r\n" +
+                                    "linktitle: Accept\r\n" +
+                                    "second_title: Aspose.Words for C++ API Reference\r\n" +
+                                    "description: 'Aspose::Words::Document::Accept method. Accepts a visitor' in C++.\r\n" +
+                                    "type: docs\r\n" +
+                                    "weight: 14\r\n" +
+                                    "url: /cpp/aspose.words/document/accept/\r\n" +
+                                    "---";
+            Assert.AreEqual(expected, generatorFunc(new Dictionary<String, String>
+            {
+                {"title", "Aspose::Words::Document::Accept method"},
+                {"linktitle", "Accept"},
+                {"description", "Aspose::Words::Document::Accept method. Accepts a visitor"},
+                {"weight", "14"},
+                {"ref", "aspose.words/document/accept"}
+            }));
+        }
+    }
+}
---
+++ Doxygen2HugoConverter/Doxygen2HugoConverter/HeaderUtils.cs
@@ -0,0 +1,94 @@
+﻿using System.Text;
+
+namespace Doxygen2HugoConverter
+{
+    internal static class HeaderUtils
+    {
+        public static IDictionary<String, String> ParseData(String source)
+        {
+            IDictionary<String, String> data = new Dictionary<String, String>();
+            foreach (String line in source.Split('\n'))
+            {
+                Int32 colonIndex = line.IndexOf(':');
+                if (colonIndex != -1)
+                {
+                    String key = line.Substring(0, colonIndex).Trim();
+                    String value = line.Substring(colonIndex + 1).Trim();
+                    data.Add(key, value);
+                }
+            }
+
+            return data;
+        }
+    }
+
+    internal static class HeaderTemplateFactory
+    {
+        public static Func<IDictionary<String, String>, String> CreateFromSource(String source)
+        {
+            StringBuilder dest = new StringBuilder();
+            IList<String> parameters = new List<String>();
+            State ProcessChar(State state, Char ch)
+            {
+                switch (state)
+                {
+                    case UsualTextState _ when ch == Eof:
+                        return state;
+                    case UsualTextState _ when ch == '$':
+                        return new ParameterState();
+                    case UsualTextState _:
+                        dest.Append(ch);
+                        return state;
+                    case ParameterState parameterState when ch == Eof:
+                        dest.Append($"{{{parameters.Count}}}");
+                        parameters.Add(parameterState.Name.ToString());
+                        return state;
+                    case ParameterState parameterState when ch == '$':
+                        dest.Append($"{{{parameters.Count}}}");
+                        parameters.Add(parameterState.Name.ToString());
+                        return new ParameterState();
+                    case ParameterState parameterState when !Char.IsLetter(ch):
+                        dest.Append($"{{{parameters.Count}}}");
+                        parameters.Add(parameterState.Name.ToString());
+                        dest.Append(ch);
+                        return new UsualTextState();
+                    case ParameterState parameterState:
+                        parameterState.Name.Append(ch);
+                        return state;
+                    default:
+                        throw new InvalidOperationException("Bad state");
+                }
+            }
+            State state = new UsualTextState();
+            foreach (Char ch in source)
+                state = ProcessChar(state, ch);
+            ProcessChar(state, Eof);
+            String template = dest.ToString();
+
+            String PrepareData(String data) => data.Contains(':') ? $"'{data}'" : data;
+            String GetResult(IDictionary<String, String> data)
+            {
+                Object[] args = new Object[parameters.Count];
+                for (Int32 index = 0; index < parameters.Count; ++index)
+                {
+                    if (!data.ContainsKey(parameters[index]))
+                        throw new InvalidOperationException("Bad data");
+                    args[index] = PrepareData(data[parameters[index]]);
+                }
+                return String.Format(template, args);
+            }
+            return GetResult;
+        }
+
+        private abstract record State;
+
+        private record UsualTextState : State;
+
+        private record ParameterState : State
+        {
+            public StringBuilder Name { get; } = new();
+        }
+
+        private const Char Eof = '\u0000';
+    }
+}
