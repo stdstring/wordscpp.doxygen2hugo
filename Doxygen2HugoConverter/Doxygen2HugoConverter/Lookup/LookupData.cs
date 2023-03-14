0d36f53c5a3174ad4af4718268ed642e876e11a0
stdstring <std_string@mail.ru>
2023-03-14 21:41:50 +0500

    added engine for dest weight data generation based on source weight data

---
+++ Doxygen2HugoConverter/Doxygen2HugoConverter/Lookup/LookupData.cs
@@ -0,0 +1,59 @@
+﻿using System.Xml.Serialization;
+
+namespace Doxygen2HugoConverter.Lookup
+{
+    [XmlRoot("LookupItem")]
+    public class LookupItem
+    {
+        public LookupItem()
+        {
+        }
+
+        public LookupItem(String name, Int32 weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+        public LookupItem(String name, Int32 weight, LookupItem[]? children)
+        {
+            Name = name;
+            Weight = weight;
+            Children = children;
+        }
+
+        [XmlAttribute("Name")]
+        public String Name { get; set; } = "";
+
+        [XmlAttribute("Weight")]
+        public Int32 Weight { get; set; }
+
+        [XmlElement("LookupItem")]
+        public LookupItem[]? Children { get; set; }
+    }
+
+    [XmlRoot("LookupData")]
+    public class LookupData
+    {
+        public LookupData()
+        {
+        }
+
+        public LookupData(Int32 delta)
+        {
+            Delta = delta;
+        }
+
+        public LookupData(Int32 delta, LookupItem[]? children)
+        {
+            Delta = delta;
+            Children = children;
+        }
+
+        [XmlAttribute("Delta")]
+        public Int32 Delta { get; set; }
+
+        [XmlElement("LookupItem")]
+        public LookupItem[]? Children { get; set; }
+    }
+}
